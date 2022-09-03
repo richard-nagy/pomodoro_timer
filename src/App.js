388218cb03990c512a017130351e0c7225c5b651
Richárd Nagy <nagy.richard@thisisdone.com>
2022-09-03 20:32:41 +0200

    Add flashing title alert

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import Clock from "./Clock";
-import Controller from "./Controller";
+import Clock from "./Components/Clock";
+import Controller from "./Components/Controller";
 
 function App() {
     const [state, setState] = useState("-");
@@ -10,16 +10,64 @@ function App() {
     const [shortBreakTime, setShortBreakTime] = useState(2);
     const [longBreakTime, setLongBreakTime] = useState(8);
     const [time, setTime] = useState(0);
+    const [tabHasFocus, setTabHasFocus] = useState(true);
+    const [newAlert, setNewAlert] = useState(false);
 
+    // Setup a listener, watches if the page is in focus
+    useEffect(() => {
+        const handleFocus = () => {
+            setTabHasFocus(true);
+            document.title = "React App";
+        };
+
+        const handleBlur = () => {
+            setTabHasFocus(false);
+        };
+
+        window.addEventListener("focus", handleFocus);
+        window.addEventListener("blur", handleBlur);
+
+        return () => {
+            window.removeEventListener("focus", handleFocus);
+            window.removeEventListener("blur", handleBlur);
+        };
+    }, []);
+
+    // If a countdown is finished, and the page is out of focus
+    // Start flashing the page title text
+    useEffect(() => {
+        if (tabHasFocus) {
+            setNewAlert(false);
+            return;
+        }
+
+        if (newAlert === false) {
+            setNewAlert(false);
+
+            return;
+        }
+
+        const alert = setInterval(() => {
+            const title1 = "React App";
+            const title2 = "🔴 Alert";
+
+            document.title === title1 ? (document.title = title2) : (document.title = title1);
+        }, 1000);
+
+        return () => clearInterval(alert);
+    }, [tabHasFocus, newAlert]);
+
+    // Time countdown
     useEffect(() => {
         if (!isTimeRunning) {
             return;
         }
 
-        const intervalId = setInterval(() => {
+        const interval = setInterval(() => {
             setTime((prevTime) => prevTime - 1);
         }, 1000);
 
+        // When time runs out, set up the next period
         if (time < 0) {
             switch (state) {
                 case "Work":
@@ -47,9 +95,10 @@ function App() {
                     setTime(workTime);
                     break;
             }
+            setNewAlert(true);
         }
 
-        return () => clearInterval(intervalId);
+        return () => clearInterval(interval);
     }, [isTimeRunning, time, state, workTime, shortBreakTime, longBreakTime, numberOfBreak]);
 
     return (
--- src/index.js
+++ src/index.js
@@ -1,35 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Components/App";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <App />
-        {/* <div>
-            <h1 />
-            <Timer />
-            <div>
-                <span> /TIMER/ </span>
-                <span> /CURRENT SESSION/ </span>
-            </div>
-            <TimerControllers />
-            <div>
-                <WorkController />
-                <div>
-                    <button> + </button>
-                    <span> /BREAKTIME/ </span>
-                    <button> - </button>
-                </div>
-                <BreakController />
-                <div>
-                    <button> + </button>
-                    <span> /WORKTIME/ </span>
-                    <button> - </button>
-                </div>
-            </div>
-            <Sound />
-            <button>/SOUND ICON/</button>
-        </div> */}
     </React.StrictMode>
 );
